54060b4fae3f799e992f2df469e1c03d1cb6f863
dosabhijith <dosabhijith@gmail.com>
2024-10-18 11:23:21 +0530

    feat: add js

--- js.js
+++ js.js
@@ -1,8 +1,8 @@
 window.addEventListener("scroll", function () {
-  var header_navbar = document.getElementById("header-navbar");
+  var headerNavbar = document.getElementById("header-navbar");
   if (this.window.scrollY > 650) {
-    header_navbar.classList.add("scrolled");
+    headerNavbar.classList.add("scrolled");
   } else {
-    header_navbar.classList.remove("scrolled");
+    headerNavbar.classList.remove("scrolled");
   }
 });
