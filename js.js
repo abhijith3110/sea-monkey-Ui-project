630f8fe0c2715210069d3613758307d5a9d73df3
dosabhijith <dosabhijith@gmail.com>
2024-10-19 20:28:35 +0530

    create: Create gallery page

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function () {
   var header_navbar = document.getElementById("header-navbar");
-  if (this.window.scrollY > 650) {
+  if (this.window.scrollY > 250) {
     header_navbar.classList.add("scrolled");
   } else {
     header_navbar.classList.remove("scrolled");
